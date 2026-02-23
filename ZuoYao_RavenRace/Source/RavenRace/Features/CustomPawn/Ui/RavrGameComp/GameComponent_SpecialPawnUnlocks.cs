83471136f99fc8572d13b7b5e01899f2f3a43818
huangqiyuan11 <huangqiyuan11@gitee.com>
2026-02-23 15:38:43 +0800

    update RavenRace special pawn unlocks

--- ZuoYao_RavenRace/Source/RavenRace/Features/CustomPawn/Ui/RavrGameComp/GameComponent_SpecialPawnUnlocks.cs
+++ ZuoYao_RavenRace/Source/RavenRace/Features/CustomPawn/Ui/RavrGameComp/GameComponent_SpecialPawnUnlocks.cs
@@ -49,8 +49,6 @@ namespace RavenRace.Features.CustomPawn.Ui.RavrGameComp
             }
         }
 
-        // ── 初始化 ───────────────────────────────────────────────
-
         public override void FinalizeInit()
         {
             // 扫描所有带扩展的 KindDef
